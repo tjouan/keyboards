06cba33e4164987d04071d72712f1c402e8cb5ce
Thibault Jouan <tj@a13.fr>
2016-01-12 21:11:41 +0000

    Add test program in `src' subdirectory

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include "Arduino.h"
+
+const int led_pin = 13;
+
+void setup() {
+  pinMode(led_pin, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(led_pin, HIGH);
+  delay(125);
+  digitalWrite(led_pin, LOW);
+  delay(125);
+}
