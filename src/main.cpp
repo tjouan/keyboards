efca34d0e20d082dae8deb55988f354a270dc5b9
Thibault Jouan <tj@a13.fr>
2016-01-13 23:30:11 +0000

    Restore original keyboard code from 2e237bb

--- src/main.cpp
+++ src/main.cpp
@@ -11,11 +11,40 @@ int __cxa_guard_acquire(__guard *g) {return !*(char *)(g);};
 void __cxa_guard_release (__guard *g) {*(char *)g = 1;};
 
 
+#define VERSION               "0.0.1"
+#define SERIAL_SPEED          115200
+#define INPUT_ROW_0           2
+#define INPUT_ROW_1           3
+#define OUTPUT_COL_0          4
+#define OUTPUT_COL_1          5
+#define ROWS_COUNT            2
+#define COLS_COUNT            2
+
+
 void setup() {
+  pinMode(INPUT_ROW_0, INPUT);
+  pinMode(INPUT_ROW_1, INPUT);
+  pinMode(OUTPUT_COL_0, OUTPUT);
+  pinMode(OUTPUT_COL_1, OUTPUT);
+
   Keyboard.begin();
 }
 
 void loop() {
-  Keyboard.print("t");
-  delay(2000);
+  int ir = 0;
+  int ic = 0;
+
+  for (ic = 0; ic < COLS_COUNT; ic += 1) {
+    digitalWrite(OUTPUT_COL_0 + ic, HIGH);
+
+    for (ir = 0; ir < ROWS_COUNT; ir += 1) {
+      if (digitalRead(INPUT_ROW_0 + ir)) {
+        Keyboard.print("t");
+      }
+    }
+
+    digitalWrite(OUTPUT_COL_0 + ic, LOW);
+  }
+
+  delay(8);
 }
