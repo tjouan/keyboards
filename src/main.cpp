3f712d8381fbb266d7c93508bda4bdadc604edf9
Thibault Jouan <tj@a13.fr>
2016-01-27 22:55:36 +0000

    Override arduino Keyboard.h header with a copy

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include "Arduino.h"
-#include "Keyboard.h"
 // FIXME: arduino-mk does not allow to change the linker, and use avr-gcc
 // instead of avr-g++.
 // Using `-fno-threadsafe-statics' link option as workaround does not work.
 // https://github.com/arduino/Arduino/commit/37ee800abdfc4b602083ef35f942d4f46917433a
 #include "arduino_mk-linker_workarounds.h"
+#include "arduino_keyboard.h"
 #include "ascii_to_dvorak.h"
 
 
